8876291442fd1b860d29b25ada32149c6a8d0fb2
luyoyo <liulurose@sina.com>
2020-03-31 23:20:59 +0800

    http请求

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
     "fmt"
+    "github.com/gin-gonic/gin"
+    "net/http"
 )
 
 func main() {
+    //注册一个路由器
     r := gin.Default()
-    r.GET("/ping", func(c *gin.Context) {
-        fmt.Println("hello world!")
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
+
+    //默认请求  http://localhost:8080/
+    r.GET("/", func(c *gin.Context) {
+        c.String(http.StatusOK, fmt.Sprintln(gin.H {"data": "hello world"}))
     })
-    r.Run() // listen and serve on 0.0.0.0:8080
+
+    //get请求 返回json格式
+    r.GET("/get", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{"action": "get", "format": "json"})
+    })
+
+    //post请求 返回json格式
+    r.POST("/post", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{"action": "post", "format": "json"})
+    })
+
+    //get请求html页面
+    r.GET("/index", func(c *gin.Context) {
+        r.LoadHTMLFiles("view/index.html")
+        c.HTML(http.StatusOK, "index.html", nil)
+    })
+
+    //运行（默认是8080端口）
+    r.Run()
 }
